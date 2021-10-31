86ee9cefbadf07a06a38b91ba87316b47e821746
Kyeoungeun Min <0406min@naver.com>
2021-10-31 18:55:41 +0900

    2021-10-31(2)

---
+++ 입출력과사칙연산/사칙연산10869/Main.java
@@ -0,0 +1,18 @@
+package 입출력과사칙연산.사칙연산10869;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a<=1||b>=10000)
+            return;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
