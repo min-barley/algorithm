3575acfd6409370e9b232c63339223ed5bc16269
Kyeoungeun Min <0406min@naver.com>
2021-10-31 18:20:25 +0900

    1단계

---
+++ 고양이10171/Main.java
@@ -0,0 +1,11 @@
+package 고양이10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
